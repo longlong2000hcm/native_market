fd35d05939a7d95f343c4b323a250bc6732d4f46
Long <longlong2000hcm@gmail.com>
2020-03-12 12:56:08 +0200

    1st layout of shop tab

--- components/Shop.js
+++ components/Shop.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 
 export default class Shop extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            productsList = [
+            searchText: "",
+            productsList: [
                 {
                     "id": 1,
-                    "title": "bike",
+                    "title": "bike 1",
                     "description": "for cycling",
                     "category": "machine",
                     "location": "Oulu",
                     "images": [
-                        "download (1).jpg"
+                        "https://cdn.shopify.com/s/files/1/2081/1519/products/1600x1067_US_B_Blue_PROFILE_1600x1067.jpg?v=1573931859"
                     ],
                     "price": "100$",
                     "dateOfPosting": "23/2/2020",
@@ -23,14 +24,14 @@ export default class Shop extends Component {
                 },
                 {
                     "id": 2,
-                    "title": "bike",
+                    "title": "bike 2",
                     "description": "for cycling",
                     "category": "machine",
                     "location": "Oulu",
                     "images": [
-                        "download (1).jpg"
+                        "https://www.rosebikes.fi/images/v7PsIuvrqBrm6SfrEscV6HwbKiS_T5yjnFBa05QXZ80/resize:fit:1800:1200:1/gravity:no/background:E5E8EB/bG9jYWw6Ly8vcHJvZHVjdC8yMjc2Mjk2XzEucG5n.jpeg"
                     ],
-                    "price": "100$",
+                    "price": "200$",
                     "dateOfPosting": "23/2/2020",
                     "deliveryType": "shipping",
                     "sellerInfo": "sellerInfo1"
@@ -38,10 +39,43 @@ export default class Shop extends Component {
             ]
         }
     }
+    handleSearchTextChange = (text) => {
+        this.setState({ searchText: text })
+    }
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Shop!</Text>
+            <View style={{ flex: 1, alignItems: 'center', marginTop: "10%" }}>
+                <TextInput
+                    style={{
+                        height: 40, borderColor: 'gray',
+                        borderWidth: 1, width: "100%", backgroundColor: "white",
+                        paddingHorizontal: "5%"
+                    }}
+                    onChangeText={text => this.handleSearchTextChange(text)}
+                    value={this.state.searchText}
+                    placeholder="Search..."
+                />
+                {this.state.productsList.map((item, index) =>
+                    <View key={index} style={{
+                        flex: 1,
+                        flexDirection: "row",
+                        margin: "5%",
+                        padding: "5%",
+                        borderColor: "gray",
+                        borderWidth: 1,
+                        backgroundColor: "white",
+                    }}>
+                        <Image
+                            style={{ flex: 1, width: 50, height: 50 }}
+                            source={{ uri: item.images[0] }}
+                        />
+                        <View style={{ flex: 1 }}>
+                            <Text >{item.title}</Text>
+                            <Text>{item.price}</Text>
+                        </View>
+                    </View>
+
+                )}
             </View>
         )
     }
